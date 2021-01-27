c35e5b40e4c642e9ebb041b7142876cd59654307
parkerlg <69094049+parkerlg@users.noreply.github.com>
2021-01-27 10:52:49 -0700

    fix

---
+++ ParkerGsFirstASPNETWebApp/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ParkerGsFirstASPNETWebApp.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace ParkerGsFirstASPNETWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("Page")]
+        public IActionResult Page ()
+        {
+            return View();
+        }
+
+        [HttpPost("Page")]
+        public IActionResult Page (PageModel model)
+        {
+            return View();
+        }
+    }
+}
---
+++ ParkerGsFirstASPNETWebApp/Models/PageModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ParkerGsFirstASPNETWebApp.Models
+{
+    public class PageModel
+    {
+        [Required]
+        [Range(0, 100)]
+        public int assignments { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int group_projects { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int quizzes { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int exams { get; set; }
+        [Required]
+        [Range(0, 100)]
+        public int intex { get; set; }
+    }
+}
